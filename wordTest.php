96f9bdf2a26863a988971a4af7ab8194b5d33c13
Alex-pc <Alex-pc@192.168.0.100>
2015-03-05 23:54:38 +0400

    Merge origin/master
    Conflicts:
    	nbproject/project.xml

--- wordTest.php
+++ wordTest.php
@@ -17,12 +17,10 @@ $php_word->addTableStyle('myTable', $tableStyle);
 $table = $setcion->addTable('myTable');
 
 $first_row = $table->addRow();
-$cell = $first_row->addCell(1000);
-$cell->addText('Район – вопрос');
-//echo "width cell = {$cell->getWidth()}";
-$first_row->addCell(1000)->addText('Кол-во новых постов');
-$first_row->addCell(1000)->addText('Из них наших информационных постов (онлайн) – т.е. кол-во информации от бот сети');
-$first_row->addCell(1000)->addText('Кол-во действия');
+$first_row->addCell(2000)->addText('Район – вопрос');
+$first_row->addCell(2000)->addText('Кол-во новых постов');
+$first_row->addCell(2000)->addText('Из них наших информационных постов (онлайн) – т.е. кол-во информации от бот сети');
+$first_row->addCell(2000)->addText('Кол-во действия');
 
 $word_writer = \PhpOffice\PhpWord\IOFactory::createWriter($php_word,'Word2007');
 $word_writer->save("hello world.docx");
