4245567a03f3c1a430cdb97d9bb71b476f62697f
Alex-pc <Alex-pc@192.168.0.100>
2015-03-05 23:53:18 +0400

    Merge origin/master
    Conflicts:
    	nbproject/project.xml

--- wordTest.php
+++ wordTest.php
@@ -7,17 +7,22 @@ $php_word = new \PhpOffice\PhpWord\PhpWord();
 $setcion = $php_word->addSection();
 
 $tableStyle = array(
-    'borderColor' => '006699',
+    'borderColor' => '000000',
     'borderSize' => 6,
-    'cellMargin' => 50
+    'cellMargin' => 50,
 );
-
-
-
+$firstRowStyle = array('width' => 'x1000');
 $php_word->addTableStyle('myTable', $tableStyle);
 
 $table = $setcion->addTable('myTable');
-$table->addRow()->addCell()->addText('Hello');
+
+$first_row = $table->addRow();
+$cell = $first_row->addCell(1000);
+$cell->addText('Район – вопрос');
+//echo "width cell = {$cell->getWidth()}";
+$first_row->addCell(1000)->addText('Кол-во новых постов');
+$first_row->addCell(1000)->addText('Из них наших информационных постов (онлайн) – т.е. кол-во информации от бот сети');
+$first_row->addCell(1000)->addText('Кол-во действия');
 
 $word_writer = \PhpOffice\PhpWord\IOFactory::createWriter($php_word,'Word2007');
-$word_writer->save("hello wordl.docx");
+$word_writer->save("hello world.docx");
